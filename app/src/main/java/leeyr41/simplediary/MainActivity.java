20230c08fb9cb738ba7c13225ce3c757be98fba7
leeyr41 <leeyr41@naver.com>
2017-05-30 20:46:55 +0900

    second commit

--- app/src/main/java/leeyr41/simplediary/MainActivity.java
+++ app/src/main/java/leeyr41/simplediary/MainActivity.java
@@ -6,11 +6,13 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
     DatePicker date;
     EditText edit;
     Button but;
-
+    String fileName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +20,24 @@ public class MainActivity extends AppCompatActivity {
         date=(DatePicker)findViewById(R.id.date_pick);
         edit=(EditText)findViewById(R.id.edit);
         but=(Button)findViewById(R.id.but);
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        final int month=cal.get(Calendar.MONTH);
+        final int day = cal.get(Calendar.DATE);
+
+        date.init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                fileName=year+"_"+(month+1)+"_"+day+".txt";
+                String readDate=readDiary(fileName);
+                edit.setText(readDate);
+                but.setEnabled(true);
+            }
+        });
+
+    }
+    public String readDiary(String fileName){
+        return null;
     }
 }
